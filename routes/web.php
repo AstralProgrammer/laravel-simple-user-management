3c19eaed9dfd58d2443e48d9806e2e186aa66c30
Angel <darkangel0938@gmail.com>
2021-06-23 20:49:59 +0800

    update login and register functionality

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/users', function () {
+    return view('users');
+})->middleware(['auth'])->name('users');
+
+require __DIR__.'/auth.php';
